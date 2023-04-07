540ef5c8a5917f1463859e9ff7afc971a24b0a0e
saradchhetri07 <saradchhetri20690@gmail.com>
2023-04-07 23:28:17 +0545

    second commit

--- Quiz app/quiz/lib/main.dart
+++ Quiz app/quiz/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import './transaction.dart';
+import 'package:quiz/widgets/userTransaction.dart';
+import 'models/transaction.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,91 +33,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<Transaction> transactions = [
-    Transaction('t1', 'new shoe', 99, DateTime.now()),
-    Transaction('t2', 'new car', 50, DateTime.now()),
-    Transaction('t3', 'new bag', 500, DateTime.now()),
-    Transaction('t4', 'new board', 60, DateTime.now()),
-  ];
+  // String? title;
+  // String? amount;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Card(
-              elevation: 10.0,
-              child: Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Container(
-                  child: Column(children: [
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Title'),
-                    ),
-                    TextField(
-                      decoration: InputDecoration(labelText: 'amount'),
-                    ),
-                    TextButton(
-                        onPressed: () {},
-                        child: Text("add amount",
-                            style: TextStyle(color: Colors.purple)))
-                  ]),
-                ),
-              ),
-            ),
-            Column(
-                children: transactions.map((tx) {
-              return Container(
-                height: 80,
-                child: Card(
-                  elevation: 10.0,
-                  color: Colors.indigo,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 15),
-                        padding:
-                            EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.white, width: 2)),
-                        child: Center(
-                            child: Text('\$${tx.amount.toString()}',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold))),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: Column(
-                          children: [
-                            Center(
-                                child: Text(tx.title.toUpperCase(),
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold))),
-                            Center(
-                                child: Text((DateFormat.yMd().format(tx.date)),
-                                    style: const TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.w200)))
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              );
-            }).toList())
-          ],
-        ),
-      ),
+      body: SingleChildScrollView(child: Center(child: userTransaction())),
       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
